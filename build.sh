f7d8d79540669c9ef6efc007ab2d730c4fe33250
JuhaniImberg <lordaku@gmail.com>
2013-08-24 00:17:32 +0300

    might work better

--- build.sh
+++ build.sh
@@ -1,51 +1,72 @@
 #!/bin/bash
 
-i=0
-x=0
-export ADD_FILE=""
-export ADD_LIB=""
+function init {
+	echo "asd"
+	i=0
+	x=0
+	status=0
+	export ADD_FILE=""
+	export ADD_LIB=""
+	return 0
+}
 
-echo "> Checking for dependencies"
+function require_event () {
+	VARS[$i]=false
+	OPTIONAL[$i]=false
+	NAME[$i]="Required | libevent"
+	FILES[$i]=""
+	LIBS[$i]=""
+	REQUIRE[$i]="/usr/include/event.h"
+	i=`expr $i + 1`
+}
 
-# event
-VARS[$i]=false
-OPTIONAL[$i]=false
-NAME[$i]="Required | libevent"
-FILES[$i]=""
-LIBS[$i]=""
-REQUIRE[$i]="/usr/include/event.h"
-i=`expr $i + 1`
-# end event
+function require_mpd () {
+	VARS[$i]=false
+	OPTIONAL[$i]=true
+	NAME[$i]="Optional | libmpdclient"
+	FILES[$i]="mpd.c"
+	LIBS[$i]="-lmpdclient"
+	REQUIRE[$i]="/usr/include/mpd/client.h"
+	i=`expr $i + 1`
+}
 
-# mpd
-VARS[$i]=false
-OPTIONAL[$i]=true
-NAME[$i]="Optional | libmpdclient"
-FILES[$i]="mpd.c"
-LIBS[$i]="-lmpdclient"
-REQUIRE[$i]="/usr/include/mpd/client.h"
-i=`expr $i + 1`
-# end mpd
+function check_deps () {
+	echo "> Checking for dependencies"
+	require_event
+	require_mpd
 
-while [ $x -le `expr $i - 1` ]
-do
-	if [ -f ${REQUIRE[$x]} ]; then
-		VARS[$x]=true
-	fi
-	echo ${NAME[$x]}": "${VARS[$x]}
-	if [ ${VARS[$x]} = true ]; then
-		ADD_LIB=$FINALLIBS" "${LIBS[$x]}
-		ADD_FILE=$FINALFILES" "${FILES[$x]}
-	else
-		if [ ${OPTIONAL[$x]} = false ]; then
-			echo "Error: Missing required library!"
-			return 1
+	while [ $x -le `expr $i - 1` ]
+	do
+		if [ -f ${REQUIRE[$x]} ]; then
+			VARS[$x]=true
 		fi
-	fi
-	x=`expr $x + 1`
-done
+		echo ${NAME[$x]}": "${VARS[$x]}
+		if [ ${VARS[$x]} = true ]; then
+			ADD_LIB=$FINALLIBS" "${LIBS[$x]}
+			ADD_FILE=$FINALFILES" "${FILES[$x]}
+		else
+			if [ ${OPTIONAL[$x]} = false ]; then
+				echo "Error: Missing required library!"
+				status=1
+				return 1
+			fi
+		fi
+		x=`expr $x + 1`
+	done
+	status=0
+	return 0
+}
 
-echo "> Compiling"
+function compile () {
+	echo "> Compiling"
 
-make clean
-make
+	make clean
+	make
+}
+
+init
+check_deps
+if [ $status = 0 ]; then
+	compile
+fi
+
