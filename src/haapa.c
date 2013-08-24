581710da8a094923d727a12737dbb46c546c1243
JuhaniImberg <lordaku@gmail.com>
2013-08-25 01:27:15 +0300

    git: stop detecting c++ plz

--- src/haapa.c
+++ src/haapa.c
@@ -18,26 +18,6 @@
 #include "battery.h"
 #include "network.h"
 
-/*
-void status_ip(char* str) {
-	int fd;
-	struct ifreq ifr;
-
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-	ifr.ifr_addr.sa_family = AF_INET;
-
-	strncpy(ifr.ifr_name, IP_INTERFACE, IFNAMSIZ-1);
-
-	ioctl(fd, SIOCGIFADDR, &ifr);
-
-	close(fd);
-	snprintf(str, SEGMENT_LENGTH,
-		"%s", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-
-}
-*/
-
 char buffer[1024];
 
 void start_segment() {
