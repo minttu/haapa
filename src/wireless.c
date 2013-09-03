0b748f967b7a1b089c7375c823a334bcf42772c8
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2013-09-04 00:38:33 +0300

    broken commit, don't use

--- src/wireless.c
+++ src/wireless.c
@@ -1,96 +1,88 @@
 #include "config.h"
-#include "result.h"
 
 #ifdef INCLUDE_IWLIB
 
+#include "result.h"
+#include "wireless.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <iwlib.h>
 
+wireless_response *response = NULL;
+static int wireless_updated = 0;
+
+void _wireless_reset() {
+    wireless_updated = 0;
+}
+
 int _wireless_update(char *ifname) {
     int sock;
     struct wireless_info info = {};
+    struct iwreq req;
+    if(!response) {
+        response = calloc(sizeof(wireless_response), 1);
+        response->essid = malloc(IW_ESSID_MAX_SIZE + 1);
+        response->int_max[0] = 1;
+        response->int_max[1] = 1;
+        response->int_max[2] = 1;
+        response->int_max[3] = 100;
+    }
     if((sock = iw_sockets_open()) < 0)
         return -1;
     iw_get_basic_config(sock, ifname, &(info.b));
-    if(iw_get_range_info(sock, ifname, &(info.range)) >= 0)
+    if(iw_get_ext(sock, ifname, SIOCGIWAP, &req)) { /* AP addr */
+        info.has_ap_addr = 1;
+        memcpy(&(info.ap_addr), &(req.u.ap_addr), sizeof(sockaddr));
+    }
+    if(iw_get_ext(sock, ifname, SIOCGIWRATE, &req) >= 0) /* bit rate */
+    {
+      info.has_bitrate = 1;
+      memcpy(&(info.bitrate), &(req.u.bitrate), sizeof(iwparam));
+    }
+
+    if(iw_get_range_info(sock, ifname, &(info.range)) >= 0) /* range */
         info.has_range = 1;
-    if(iw_get_stats(sock, ifname, &(info.stats), &info.range, info.has_range) >= 0)
+    if(iw_get_stats(sock, ifname, &(info.stats),
+                          &info.range, info.has_range) >= 0)
         info.has_stats = 1;
+
+    if(info.b.has_freq) {
+        if(info.has_range && info.b.freq < 1000) {
+            response->channel = iw_channel_to_freq(
+                            (int)info.b.freq, &response->freq, &info.range);
+            if(response->channel<0)
+                response->channel = 0;
+                response->freq = 0;
+        }
+        else {
+            response->channel = -1;
+            response->freq = info.b.freq;
+        }
+    }
+    strncpy(response->essid, info.b.essid, IW_ESSID_MAX_SIZE - 1);
+    if(info.has_stats)
+        response->quality = info.stats.qual.qual;
+    wireless_updated = 0;
     return 0;
 }
 
 /* todo: general _wireless_init() */
-Result *wireless_essid(char *str) {
-    struct iwreq *val;
-    int sock;
+Result *wireless_essid(char *ifname) {
     Result *res;
-
+    if(!wireless_updated)
+        _wireless_update(ifname);
     res = init_res();
-    val = calloc(sizeof(struct iwreq), 1);
-    strcat(val->ifr_name, str);
-    if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        free(val);
-        res->error = -1;
-        return res;
-    }
-    val->u.essid.pointer = calloc(sizeof(char) * IW_ESSID_MAX_SIZE + 1, 1);
-    val->u.essid.length = IW_ESSID_MAX_SIZE;
-
-    if(ioctl(sock, SIOCGIWESSID, val) < 0) {
-        free(val->u.essid.pointer);
-        free(val);
-        close(sock);
-        res->error = -1;
-        return res;
-    }
-
-    strcat(res->string, val->u.essid.pointer);
-    close(sock);
-    free(val->u.essid.pointer);
-    free(val);
+    strncat(res->string, response->essid, sizeof(res->string) - 1);
     return res;
 }
 
-Result *wireless_signal(char *str) {
-    struct iwreq *val;
-    int sock;
+Result *wireless_signal(char *ifname) {
     Result *res;
-
+    if(!wireless_updated)
+        _wireless_update(ifname);
     res = init_res();
-    val = calloc(sizeof(struct iwreq), 1);
-    strcat(val->ifr_name, str);
-    if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        free(val);
-        res->error = -1;
-        return res;
-    }
-    val->u.essid.pointer = calloc(sizeof(char) * IW_ESSID_MAX_SIZE + 1, 1);
-    val->u.essid.length = IW_ESSID_MAX_SIZE;
-
-    if(ioctl(sock, SIOCGIWESSID, val) < 0) {
-        free(val->u.essid.pointer);
-        free(val);
-        close(sock);
-        res->error = -1;
-        return res;
-    }
-
-    strcat(res->string, val->u.essid.pointer);
-    close(sock);
-    free(val->u.essid.pointer);
-    free(val);
-    return res;
-}
-#else
-Result *wireless_essid() {
-    Result *res = init_res();
-    res->error = -1;
-    return res;
-}
-Result *wireless_signal() {
-    Result *res = init_res();
-    res->error = -1;
+    res->max = response->q_max;
+    res->value = response->quality;
     return res;
 }
 #endif
--- src/wireless.h
+++ src/wireless.h
@@ -3,7 +3,28 @@
 
 #include "result.h"
 
-Result *wireless_essid(char *str);
+typedef struct wireless_response {
+    char *essid;
+    char *ap_addr;
+    double freq;
+    int channel;
+    int bitrate;
+    int quality;
+    union {
+        struct {
+            int freq_max;
+            int chan_max;
+            int bit_max;
+            int q_max;
+        };
+        int int_max[4];
+    };
+} wireless_response;
+
+void _wireless_reset();
+int _wireless_update();
+Result *wireless_essid();
+Result *wireless_signal();
 
 #define wireless_github_stop_detecting_cpp_please
 
