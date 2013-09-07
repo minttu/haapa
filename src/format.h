a0fa32d344b3d1625cc0881bf463a3a1ddad5284
JuhaniImberg <lordaku@gmail.com>
2013-09-07 18:12:13 +0300

    Added macros i3_manual_start() and i3_manual_end(color)

--- src/format.h
+++ src/format.h
@@ -20,6 +20,9 @@ Format *format_x256();
 #define i3_manual_s "!i3_manual_start"
 #define i3_manual_e "!i3_manual_end"
 
+#define i3_manual_start() {string, text, i3_manual_s, "", is_format, "i3_manual"}
+#define i3_manual_end(color) {string, text, i3_manual_e, color, is_format, "i3_manual"}
+
 int is_format(char *str);
 
 #endif
